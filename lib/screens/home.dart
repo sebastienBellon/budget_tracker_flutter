3c2f8929e3d92b544026543513ba266653abd371
Sébastien Bellon <seb.bellon@protonmail.com>
2022-07-17 19:20:49 +0200

    ✨ add a bottom navigation bar that is inside a statefull widget and render page based on user selection

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,25 +1,43 @@
+import 'package:budget_tracker/pages/home_page.dart';
+import 'package:budget_tracker/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<Home> createState() => _HomeState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeState extends State<Home> {
   List<BottomNavigationBarItem> bottomNavItems = const [
     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
     BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
   ];
+
+  List<Widget> pages = const [
+    HomePage(),
+    ProfilePage(),
+  ];
+
+  int _currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Budget Tracker'),
       ),
-      body: Container(),
-      bottomNavigationBar: BottomNavigationBar(items: bottomNavItems),
+      body: pages[_currentPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        items: bottomNavItems,
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+      ),
     );
   }
 }
